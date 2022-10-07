088ba1c63351132fc2ac3249d8a06f0ff32f6f47
Pranith chowdary Karumanchi <pranithtpm@gmail.com>
2022-10-07 03:21:41 +0000

    connected to MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,20 @@
-// require database connection 
-const dbConnect = require("./db/dbConnect");
+import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
 
-// execute database connection 
-dbConnect();
+const app = express()
+dotenv.config()
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URL);
+        console.log("Connected to mongoDB..")
+    } catch (error) {
+        throw error;
+    }
+};
+
+app.listen(8000, ()=> {
+    console.log("connected to backend server..")
+    connect()
+})

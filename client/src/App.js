e25dde055a1f4bc5c4d528e699a69190f1df9ec4
Pranith chowdary Karumanchi <pranithtpm@gmail.com>
2022-10-10 13:37:33 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Intro from "./pages/Intro";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/home";
+import Admin from "./pages/Admin";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Intro/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/Admin" element={<Admin/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
